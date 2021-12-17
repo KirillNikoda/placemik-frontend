9211cdf029115f75c845962cfaa103197759a4db
Sergey <kurilovsergey15@gmail.com>
2021-12-17 23:37:13 +0200

    hotfix/fix error with types

--- karma.conf.js
+++ karma.conf.js
@@ -37,11 +37,11 @@ module.exports = function (config) {
     colors: true,
     logLevel: config.LOG_INFO,
     autoWatch: true,
-    browsers: ['Chrome'],
+    browsers: ['ChromeHeadless', 'Firefox'],
     customLaunchers: {
-      ChromeHeadlessCustom: {
+      ChromeHeadlessCI: {
         base: 'ChromeHeadless',
-        flags: ['--no-sandbox', '--disable-gpu']
+        flags: ['--no-sandbox']
       }
     },
     coverageIstanbulReporter: {
